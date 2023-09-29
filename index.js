268964550e7db0beaf3870f231fda98d5dde8a6c
asem2140 <gustavoadrianpazos@gmail.com>
2023-09-29 15:26:10 -0500

    user backend updated

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
-const { dbConnection } = require('./database/config')
+const { dbConnection } = require("./database/config");
 
-const cors = require('cors')
+const cors = require("cors");
 
 // Crear servidor express
-const app = express()
+const app = express();
 
 // Base de datoss
-dbConnection()
+dbConnection();
 
 // CORS
-app.use(cors())
+app.use(cors());
 
 // Lectua y parseo del body
-app.use(express.json())
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth.route'))
-app.use('/api/users', require('./routes/user.route'))
-app.use('/api/posts', require('./routes/post.route'))
-app.use('/api/comments', require('./routes/comment.route'))
-app.use('/api/communitys', require('./routes/community.route'))
-app.use('/api/chat', require('./routes/chat.route'))
-app.use('/api/message', require('./routes/message.route'))
+app.use("/api/auth", require("./routes/auth.route"));
+app.use("/api/users", require("./routes/user.route"));
+app.use("/api/posts", require("./routes/post.route"));
+app.use("/api/comments", require("./routes/comment.route"));
+app.use("/api/communitys", require("./routes/community.route"));
+app.use("/api/chat", require("./routes/chat.route"));
+app.use("/api/message", require("./routes/message.route"));
 
 // Escuchas peticines
 const server = app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
-})
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
 
 // socket.io
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000'
-  }
-})
+    origin: "http://localhost:3000",
+  },
+});
 
-io.on('connection', (socket) => {
-  console.log('Connected to socket.io')
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
 
   // Creating a new socket where the frontend will send some data and will join a room
-  socket.on('setup', (userData) => {
-    socket.join(userData._id)
-    console.log(userData._id)
-    socket.emit('connected')
-  })
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log(userData._id);
+    socket.emit("connected");
+  });
 
-  socket.on('join chat', (room) => {
-    socket.join(room)
-    console.log('User Joined Room: ' + room)
-  })
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
 
-  socket.on('typing', (room) => {
-    socket.in(room).emit('typing')
-  })
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
 
-  socket.on('stop typing', (room) => {
-    socket.in(room).emit('stop typing')
-  })
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
 
-  socket.on('new message', (newMessageRecieved) => {
-    const chat = newMessageRecieved.chat
-    if (!chat.users) return console.log('chat.users not defined')
+  socket.on("new message", (newMessageRecieved) => {
+    const chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("chat.users not defined");
 
-    chat.users.forEach(user => {
-      if (user._id === newMessageRecieved.sender._id) return
-      socket.in(user._id).emit('message recieved', newMessageRecieved)
-    })
-  })
-})
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+});
